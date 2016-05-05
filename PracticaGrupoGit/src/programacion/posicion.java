acb8ce010d1748045aab563fddf19986d0786387
antonia <antonia@Zapata>
2016-05-05 17:41:23 +0200

    # WARNING: head commit changed in the meantime
    tyjty

---
+++ PracticaGrupoGit/src/programacion/posicion.java
@@ -0,0 +1,38 @@
+package programacion;
+
+/**
+ * COnstructor copia.
+ * Establece el valor incioal de cada uno de
+ * los valores obtenidos de un objeto de su
+ */
+
+
+
+public Posicion (Posicion p) {
+	this(p.x, p.y);
+}
+
+
+
+
+
+/**
+ * @return the x
+ */
+
+public int getX(){
+	
+	
+	return x;
+}
+
+/**
+ * @return the y
+ */
+
+public int getY(){
+	
+	return y;
+
+
+}
